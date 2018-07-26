fdf2510f8efba5c80c9234de7ed462036ea19694
andreea-vlad <simina.andreea.vlad@gmail.com>
2018-07-26 20:29:54 +0100

    modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -157,3 +157,113 @@ $("#radio-inline-someone").change(function() {
 $("#radio-inline-myself").change(function() {
 	$(".thirdPartyDetails").addClass("visually-hidden");
 });
+
+// Show Dialog
+
+function showDialog() {
+
+    if ($('.dialog').length > 0) {
+
+        var dialogData = {
+            lastFocus: null
+        };
+
+        $('a[data-toggle=dialog]').on('click', function(e) {
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            var anchor = $(this);
+            var data = '#' + anchor.attr('data-target');
+
+            openDialog(data, anchor); // Pass data value into function
+
+        });
+
+        // Open dialog
+
+        function openDialog(data, anchor) {
+
+            dialogData.lastFocus = anchor;
+
+            var dialog = $(data);
+
+            dialog.attr('aria-hidden', 'false')
+                .find('.dialog-content').focus()
+                .attr('tabindex', '-1');
+
+            dialog.trap();
+        }
+
+        // Close dialog only if visible
+
+        function closeDialog() {
+
+            var dialog = $('.dialog[aria-hidden=false]');
+
+            dialog.attr('aria-hidden', 'true')
+                .find('.dialog-content').blur()
+                .attr('tabindex', '0');
+
+            dialog.untrap();
+
+            dialogData.lastFocus.focus();
+            dialogData.lastFocus.blur();
+        }
+
+        // Stop bubbling
+
+        $('.dialog-holder').on('click', function(e) {
+
+            e.stopPropagation();
+
+        });
+
+        $('.dialog-close').on('click', function(e) {
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            closeDialog();
+
+        });
+
+        $('.dialog-cancel').on('click', function(e) {
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            closeDialog();
+
+        });
+
+        // Document binding events
+
+        $(document).bind({
+
+            click: function(e) {
+
+                closeDialog();
+
+            },
+
+            keyup: function(e) {
+
+                if (e.keyCode == 27) {
+
+                    closeDialog();
+
+                }
+
+            }
+
+        });
+
+    }
+
+}
+
+// Document ready
+(function() {
+  showDialog();
+})();
